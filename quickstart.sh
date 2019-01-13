e3ee7213b0d08c5f87896cef745ed91d140d956b
igetit41 <45726571+igetit41@users.noreply.github.com>
2019-01-12 19:55:37 -0800

    autobuild

--- quickstart.sh
+++ quickstart.sh
@@ -1,23 +1,18 @@
-cd ~
-
-gcloud container clusters create trafficcollection --machine-type=n1-standard-1 --zone=us-central1-f --enable-autoscaling --min-nodes=2 --max-nodes=10 --num-nodes=3
-
-kubectl create clusterrolebinding myclusteradmin --clusterrole=cluster-admin --user=<GCP user email address>
+#Declare Environment Variables
+export gcpuser=<GCP user email address>
 
 export rabbitServer=rabbitmq.default.svc.cluster.local
 export rabbitServer_mqadmin=<admin username>
 export rabbitServer_mqadminpassword=<admin password>
-
 
 export stream_url=data.cityofchicago.org
 export stream_account=<account>
 export stream_password=<password>
 export stream_token=<token>
 
-
 export postgreSQL_user=<username>
 export postgreSQL_password=<password>
-export postgreSQL_database=<database name>
+export postgreSQL_database=postgres
 
 export postgreSQL_host=<server IP>
 export postgreSQL_pathclientcert=~/git/Credentials/gcppostgresslclient-cert.pem
@@ -25,7 +20,20 @@ export postgreSQL_pathclientkey=~/git/Credentials/gcppostgresslclient-key.pem
 export postgreSQL_pathserverca=~/git/Credentials/gcppostgresslserver-ca.pem
 export bigquery_pathcreds=~/git/Credentials/bigquerycreds.json
 
+export rabbitmqrbacFile=~/git/rabbitmq-cluster/rabbitmq_rbac.yaml
+export rabbitmqssFile=~/git/rabbitmq-cluster/rabbitmq_statefulsets.yaml
+export dbpbigqFile=~/git/pythonDBPopulate-bigquery/dep-dbpbigq.yaml
+export dbppgsqlFile=~/git/pythonDBPopulate-postgresql/dep-dbppgsql.yaml
+export datadistFile=~/git/datadist-postgresql/dep-datadist.yaml
+export strconsFile=~/git/pythonStreamConsume/dep-strcons.yaml
 
+export rabbitmqImage=gcr.io/$DEVSHELL_PROJECT_ID/rabbitmq:v1
+export bigqueryImage=gcr.io/$DEVSHELL_PROJECT_ID/dbpbigq:v1
+export dbppgsqlImage=gcr.io/$DEVSHELL_PROJECT_ID/dbppgsql:v1
+export datadistImage=gcr.io/$DEVSHELL_PROJECT_ID/datadist:v1
+export strconsImage=gcr.io/$DEVSHELL_PROJECT_ID/strcons:v1
+
+#Move Credentials
 cp $postgreSQL_pathclientcert ~/git/pythonDBPopulate-postgresql/gcppostgresslclient-cert.pem
 cp $postgreSQL_pathclientkey ~/git/pythonDBPopulate-postgresql/gcppostgresslclient-key.pem
 cp $postgreSQL_pathserverca ~/git/pythonDBPopulate-postgresql/gcppostgresslserver-ca.pem
@@ -34,33 +42,12 @@ cp $postgreSQL_pathclientcert ~/git/datadist-postgresql/gcppostgresslclient-cert
 cp $postgreSQL_pathclientkey ~/git/datadist-postgresql/gcppostgresslclient-key.pem
 cp $postgreSQL_pathserverca ~/git/datadist-postgresql/gcppostgresslserver-ca.pem
 
-cp $postgreSQL_pathclientcert ~/git/pythonWeb/app/gcppostgresslclient-cert.pem
-cp $postgreSQL_pathclientkey ~/git/pythonWeb/app/gcppostgresslclient-key.pem
-cp $postgreSQL_pathserverca ~/git/pythonWeb/app/gcppostgresslserver-ca.pem
-
 cp $bigquery_pathcreds ~/git/pythonDBPopulate-bigquery/bigquerycreds.json
 
+#Create role binding to allow rabbit clustering
+kubectl create clusterrolebinding myclusteradmin --clusterrole=cluster-admin --user=$gcpuser
 
-cp $postgreSQL_pathclientcert ~/git/pythontest/gcppostgresslclient-cert.pem
-cp $postgreSQL_pathclientkey ~/git/pythontest/gcppostgresslclient-key.pem
-cp $postgreSQL_pathserverca  ~/git/pythontest/gcppostgresslserver-ca.pem
-cp $bigquery_pathcreds ~/git/pythontest/bigquerycreds.json
-
-export rabbitmqrbacFile=~/git/rabbitmq-cluster/rabbitmq_rbac.yaml
-export rabbitmqssFile=~/git/rabbitmq-cluster/rabbitmq_statefulsets.yaml
-export dbpbigqFile=~/git/pythonDBPopulate-bigquery/dep-dbpbigq.yaml
-export dbppgsqlFile=~/git/pythonDBPopulate-postgresql/dep-dbppgsql.yaml
-export datadistFile=~/git/datadist-postgresql/dep-datadist.yaml
-export strconsFile=~/git/pythonStreamConsume/dep-strcons.yaml
-export fullenvFile=~/git/fullenvironment.yaml
-
-export rabbitmqImage=gcr.io/$DEVSHELL_PROJECT_ID/rabbitmq:v1
-export bigqueryImage=gcr.io/$DEVSHELL_PROJECT_ID/dbpbigq:v1
-export dbppgsqlImage=gcr.io/$DEVSHELL_PROJECT_ID/dbppgsql:v1
-export datadistImage=gcr.io/$DEVSHELL_PROJECT_ID/datadist:v1
-export strconsImage=gcr.io/$DEVSHELL_PROJECT_ID/strcons:v1
-
-
+#Create images
 cd ~/git/rabbitmq
 docker rmi $rabbitmqImage -f
 docker build -f ./Dockerfile -t $rabbitmqImage .
@@ -86,7 +73,7 @@ docker rmi $strconsImage -f
 docker build -f ./Dockerfile -t $strconsImage .
 gcloud docker -- push $strconsImage
 
-
+#Build Environment
 cd ~
 envsubst < $rabbitmqrbacFile | kubectl apply -f -
 envsubst < $rabbitmqssFile | kubectl apply -f -
@@ -99,6 +86,7 @@ envsubst < $dbpbigqFile | kubectl apply -f -
 sleep 60
 envsubst < $strconsFile | kubectl apply -f -
 
+#Display environment
 kubectl get ConfigMaps
 kubectl get HorizontalPodAutoscalers
 kubectl get Services
